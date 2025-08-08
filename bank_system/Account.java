94291393c05d07453e12c85c00ea1181e34a2f50
Honey R Shetty <honeyshetty2003@gmail.com>
2025-08-08 12:44:27 +0530

    day-2-dlt

--- bank_system/Account.java
+++ bank_system/Account.java
@@ -1,47 +1,30 @@
 package bank_system;
 
-public class Account {
-    private static int counter = 1001; 
-    private int accountNumber;
-    private String personName;
-    private double balance;
+class Account {
+    int accountNumber;
+    double balance;
+    Customer customer;
 
-    public Account(String owner, double balance) {
-        this.accountNumber = counter++;
-        this.personName = owner;
-        this.balance = balance;
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getOwner() {
-        return personName;
-    }
-
-    public double getBalance() {
-        return balance;
+    public Account(int accountNumber, Customer customer) {
+        this.accountNumber = accountNumber;
+        this.customer = customer;
+        this.balance = 0;
     }
 
     public void deposit(double amount) {
-        if (amount > 0)
-            balance += amount;
-        else
-            System.out.println("Invalid amount");
+        balance += amount;
     }
 
-    public boolean withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
+    public void withdraw(double amount) {
+        if (balance >= amount) {
             balance -= amount;
-            return true;
+        }else {
+        	System.out.println("Insufficient balance in the account...");
         }
-        System.out.println("Insufficient balance...");
-        return false;
     }
-
-    public void display() {
-        System.out.println("Account Number: " + accountNumber + ", Owner: " + personName + ", Balance: " + balance);
+    public void printStatement() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Customer: " + customer.name);
+        System.out.println("Balance: " + balance);
     }
 }
-
